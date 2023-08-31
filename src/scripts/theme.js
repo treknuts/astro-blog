8b06fd1edfbbe8575e12a08b2bc2bb4c4313754e
Trevor Knutson <trevor.a.knutson@ndsu.edu>
2023-08-31 08:53:25 -0500

    Fixed theme toggle with ViewTransitions. Probably need some cleanup.

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,29 +1,27 @@
-var theme = (() => {
-  if (typeof localStorage !== "undefined" && localStorage.getItem("theme")) {
-    return localStorage.getItem("theme");
-  }
-  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    return "dark";
-  }
-  return "light";
-})();
-
-if (theme === "light") {
-  document.documentElement.classList.remove("dark");
-} else {
-  document.documentElement.classList.add("dark");
-}
-
-window.localStorage.setItem("theme", theme);
-
 // from Astro docs
-// const setDarkMode = () => {
-//   if (localStorage.darkMode) {
-//     document.documentElement.dataset.dark = '';
-//   }
-// };
+const setDarkMode = () => {
+  var theme = "light";
+  console.log(localStorage.getItem("theme"));
+  if (localStorage.getItem("theme")) {
+    console.log("Theme was defined! Got ", localStorage.getItem("theme"));
+    theme = localStorage.getItem("theme");
+  } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    console.log("The theme was not set, but the user prefers dark mode");
+    theme = "dark";
+  } else {
+    console.log("Defaulting to light mode.");
+  }
 
-// // Runs on initial navigation
-// setDarkMode();
-// // Runs on view transitions navigation
-// document.addEventListener('astro:after-swap', setDarkMode);
+  if (theme === "light") {
+    document.documentElement.setAttribute("data-theme", "light");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+  }
+
+  window.localStorage.setItem("theme", theme);
+};
+
+// Runs on initial navigation
+setDarkMode();
+// Runs on view transitions navigation
+document.addEventListener("astro:after-swap", setDarkMode);
