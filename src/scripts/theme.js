dd599dc5aabd505558c15559733dd0bc495e008e
Trevor Knutson <trevor.a.knutson@ndsu.edu>
2023-08-31 09:38:44 -0500

    Fixed mobile menu toggle.

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,15 +1,10 @@
 // from Astro docs
 const setDarkMode = () => {
   var theme = "light";
-  console.log(localStorage.getItem("theme"));
   if (localStorage.getItem("theme")) {
-    console.log("Theme was defined! Got ", localStorage.getItem("theme"));
     theme = localStorage.getItem("theme");
   } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    console.log("The theme was not set, but the user prefers dark mode");
     theme = "dark";
-  } else {
-    console.log("Defaulting to light mode.");
   }
 
   if (theme === "light") {
